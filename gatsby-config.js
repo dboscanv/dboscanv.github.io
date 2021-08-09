c8dab12abbaf9216ed9e95cce1335a9e67fe0cf1
Diego Boscan <diego.boscan@mercadolibre.com>
2021-08-08 23:58:28 -0300

    Fix background

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,7 +116,7 @@ module.exports = {
         short_name: `Diego Boscan`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#30d065`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
