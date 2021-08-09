6b946f9ac476d10971f8e1784ce1ab0a10cad8cb
Diego Boscan <diego.boscan@mercadolibre.com>
2021-08-09 00:43:14 -0300

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,6 +123,15 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-123744687-1", // Google Analytics / GA
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
